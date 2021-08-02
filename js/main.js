4a23858e2a578a39a9aba62f02d299dc37cfc589
hartpableo <hart.hpwebdev@gmail.com>
2021-08-02 20:49:34 +0800

    tweaks

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
 // Auto-update Year in Footer
 const year = document.querySelector('.year');
 year.innerHTML = new Date().getFullYear();
+
+// Client Feedback Slider
+let feedbacks = document.querySelectorAll('.client');
+
+let counter = 0;
+
+function autoCarousel() {
+    counter++;
+
+    if (counter < 0) {
+        counter = feedbacks.length - 1;
+    } else if (counter > feedbacks.length - 1) {
+        counter = 0;
+    };
+
+    feedbacks.forEach(function(feedback) {
+        feedback.style.transform = `translateX(-${counter * 100}%)`;
+    });
+}
+
+// Set Interval For Feedback Slider
+function feedbacksInterval() {
+    setInterval(autoCarousel, 5500);
+}
+
+// Run Feedback Slider
+window.addEventListener('load',function() {
+    feedbacksInterval();
+});
